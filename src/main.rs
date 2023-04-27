a2537b3c42945d91487c96102492fdcf5be97875
Simon Hausmann <hausmann@gmail.com>
2023-04-27 08:36:39 +0200

    better output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 fn main() {
     let mut db = fontdb::Database::new();
     db.load_system_fonts();
-    println!("{}", db.family_name(&fontdb::Family::SansSerif));
+    let mut query = fontdb::Query::default();
+    query.families = &[fontdb::Family::SansSerif];
+    match db.query(&query) {
+        Some(id) => {
+            println!(
+                "query for sans-serif returned: {:#?}",
+                db.face(id).as_ref().map(|f| &f.source)
+            );
+        }
+        None => println!("Could not locate default sans serif font!"),
+    }
 }
